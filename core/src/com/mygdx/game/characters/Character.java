f80b4c133d745deba2c3250219b91f76ebd51043
XChronosCS <eezor1@binghamton.edu>
2021-03-30 16:00:58 -0400

    Finally finished project after a lot of work

--- core/src/com/mygdx/game/characters/Character.java
+++ core/src/com/mygdx/game/characters/Character.java
@@ -1,17 +1,67 @@
 package com.mygdx.game.characters;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.Dimension;
 import com.mygdx.game.Entity;
+import com.mygdx.game.utils.Utils;
+
+import sun.rmi.runtime.Log;
 
 public abstract class Character extends Entity {
 
     private Texture characterArt;
+    private Texture baseArt;
+    private final float ACCELERATION = 5;
+    private float BASE_SPEED;
+    private int score = 0;
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void incrementSpeed(){
+        this.speed += ACCELERATION;
+        score++;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void resetSpeed(){
+        this.speed = BASE_SPEED;
+        score = 0;
+    }
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
     private float speed;
     private Vector2 position;
+
+    @Override
+    public float getXVal(){
+        return this.dim.x;
+    }
+
+    @Override
+    public float getYVal(){
+        return this.dim.y;
+    }
+
+    public void setPosition(Vector2 position) {
+        this.position = position;
+    }
+
+    public void setAngle(float angle) {
+        this.angle = angle;
+    }
+
     private float angle;
     private Dimension dim;
     //private Projectile projectile; //for future updates
@@ -20,17 +70,25 @@ public abstract class Character extends Entity {
         super(dimension.x, dimension.y);
         this.dim = dimension;
         this.speed = speedVal;
+        this.BASE_SPEED = speedVal;
         position = new Vector2(dimension.x, dimension.y);
         //this.projectile = bullet;
     }
     public abstract void moveCharacter(float newX, float newY);
 
-    public void setCharacterArt(Texture art){this.characterArt = art;}
+    public void setCharacterArt(Texture art){
+        this.characterArt = art;
+        this.baseArt = art;
+    }
+
+    public void updateDimension(Dimension dim){
+        this.dim = dim;
+    }
 
 
 
     public void render(SpriteBatch batch){
-        batch.draw(this.characterArt, this.getXVal(), this.getYVal(), this.getWidth(), this.getHeight());
+        Utils.drawImg(batch, this.characterArt, this.dim);
     }
 
     public Texture getCharacterArt() {
@@ -67,7 +125,7 @@ public abstract class Character extends Entity {
         return position.y + characterArt.getHeight()/(float)2;
     }
 
-    private Vector2 getOrigin(){
+    public Vector2 getOrigin(){
         return new Vector2(this.getXCenter(), this.getYCenter());
     }
 
@@ -75,29 +133,27 @@ public abstract class Character extends Entity {
         return angle;
     }
 
-    public void update(SpriteBatch batch, Vector2 target){
-        Vector2 followPath = target.cpy().sub(getOrigin());
-        followPath.nor();
-        Vector2 velocity = followPath.scl(speed);
-        angle = velocity.angleDeg();
-        position.add(velocity);
-        batch.draw(this.getCharacterArt(),
-                this.getXVal(),
-                this.getYVal(),
-                this.getWidth()/2,
-                this.getHeight()/2,
-                this.getWidth(),
-                this.getHeight(),
-                1,
-                1,
-                getAngle(),
-                0,
-                0,
-                (int) this.getWidth(),
-                (int) this.getHeight(),
-                false,
-                false);
+    public void rotate(int direction){
+        switch(direction){
+            case 0:
+                this.characterArt = baseArt;
+                //Gdx.app.debug("Case 0", "Case 0 works fine.");
+                break;
+            case 1:
+                this.characterArt = new Texture("Characters/FlameGuyBack.png");
+                //Gdx.app.debug("Case 1", "Case 1 works fine.");
+                break;
+            case 2:
+                this.characterArt = new Texture("Characters/FlameGuyUp.png");
+               // Gdx.app.debug("Case 2", "Case 2 works fine.");
+                break;
+            case 3:
+                this.characterArt = new Texture("Characters/FlameGuyDown.png");
+               // Gdx.app.debug("Case 3", "Case 3 works fine.");
+                break;
+        }
     }
+
 
 
 
